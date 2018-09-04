d7a9edaf6ac7df81f9a86d64152a7877cd7b7b77
Avni <walia.avni@gmail.com>
2018-09-04 10:36:44 -0400

    scrum db

--- scrum/views/ScrumDb.py
+++ scrum/views/ScrumDb.py
@@ -4,7 +4,8 @@ def insert_user(user):
 	'''Insert user into database.'''
 	database = get_db()
 	cursor = database.cursor()
-	cursor.execute('INSERT INTO Users (first_name, last_name, email, password) VALUES (Ash, Varma, ashvarma@umich.edu, password)')
+	cursor.execute('INSERT INTO Users (first_name, last_name, email, password) VALUES ({})'.format(user['first_name'], 
+		user['last_name'], user['email'], user['password']))
 	database.commit()
 
 def get_all_from_table(table_name):
@@ -15,10 +16,22 @@ def get_all_from_table(table_name):
 	return result.fetchall()
 
 # insert_row_into_table('Users', ['first_name', 'last_name', 'email'], ['Ash', 'Varma', 'ashvarma@umich.edu']) 
+# ['first_name', 'last_name', 'email'] --> 'first_name, last_name, email'
 def insert_row_into_table(table_name, columns, data):
 	'''Insert data into table_name in specified columns.'''
 	database = get_db()
 	cursor = database.cursor()
+	cursor.execute('INSERT INTO {} ({}) VALUES ({})'.format(table_name, col_builder(columns), data_builder(data)))
+	database.commit()
+
+def col_builder(columns):
+	a = ''
+	num_elements = len(columns)
+	for elt in columns[0:num_elements-1:]:
+		a += elt
+		a += ', '
+	return a
+
 
 '''
 Create a function that lets you update a table
